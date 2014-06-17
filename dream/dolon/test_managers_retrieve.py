b7f4424794ff27910f490f58f4e11f23cb0ea1e3
Erick <erick.peirson@asu.edu>
2014-06-17 16:02:56 -0700

    cleanup

--- dream/dolon/test_managers_retrieve.py
+++ dream/dolon/test_managers_retrieve.py
@@ -8,56 +8,50 @@ imageurl = 'http://wendycarrillo.files.wordpress.com/2010/11/antoine-dodsen-drea
 contexturl = 'http://wendycarrillo.wordpress.com/2010/11/24/fox-news-attacks-the-dream-act/'
 
 
-#class TestImageRetriever(TestCase):
-#    def setUp(self):
-#        self.R = ImageRetriever()
-#    
-#    def test_retrieveThumbnail(self):
-#        """
-#        Should return a :class:`.Thumbnail` instance.
-#        
-#        TODO: check that a real image has been downloaded?
-#        """
-#
-#        result = self.R.retrieveThumbnail(thumburl)
-#        self.assertIsInstance(result, Thumbnail)
-#        self.assertEqual(result.url, thumburl)
-#        self.assertGreater(result.height, 0)
-#        self.assertGreater(result.width, 0)
-#        self.assertGreater(result.size, 0)
-#        self.assertIsInstance(result.mime, str)
-#        self.assertGreater(len(result.mime), 0)
-#
-#    def test_retrieveImage(self):
-#        """
-#        Should return a :class:`.Thumbnail` instance.
-#        
-#        TODO: check that a real image has been downloaded?
-#        """
-#
-#        result = self.R.retrieveImage(imageurl)
-#        self.assertIsInstance(result, Image)
-#        self.assertEqual(result.url, imageurl)
-#        self.assertGreater(result.height, 0)
-#        self.assertGreater(result.width, 0)
-#        self.assertGreater(result.size, 0)
-#        self.assertIsInstance(result.mime, str)
-#        self.assertGreater(len(result.mime), 0)
-#
-#    def test_retrieveContext(self):
-#        """
-#        Should return a :class:`.Context` instance.
-#        """
-#
-#        result = self.R.retrieveContext(contexturl)
-#
-#        self.assertIsInstance(result, Context)
-#        self.assertEqual(result.url, contexturl)
-#        self.assertGreater(len(result.content), 0)
-#        self.assertGreater(len(result.title), 0)
-#
-##class
-#
-#
-#
-## Create your tests here.
+class TestImageRetriever(TestCase):
+    def setUp(self):
+        self.R = ImageRetriever()
+    
+    def test_retrieveThumbnail(self):
+        """
+        Should return a :class:`.Thumbnail` instance.
+        
+        TODO: check that a real image has been downloaded?
+        """
+
+        result = self.R.retrieveThumbnail(thumburl)
+        self.assertIsInstance(result, Thumbnail)
+        self.assertEqual(result.url, thumburl)
+        self.assertGreater(result.height, 0)
+        self.assertGreater(result.width, 0)
+        self.assertGreater(result.size, 0)
+        self.assertIsInstance(result.mime, str)
+        self.assertGreater(len(result.mime), 0)
+
+    def test_retrieveImage(self):
+        """
+        Should return a :class:`.Thumbnail` instance.
+        
+        TODO: check that a real image has been downloaded?
+        """
+
+        result = self.R.retrieveImage(imageurl)
+        self.assertIsInstance(result, Image)
+        self.assertEqual(result.url, imageurl)
+        self.assertGreater(result.height, 0)
+        self.assertGreater(result.width, 0)
+        self.assertGreater(result.size, 0)
+        self.assertIsInstance(result.mime, str)
+        self.assertGreater(len(result.mime), 0)
+
+    def test_retrieveContext(self):
+        """
+        Should return a :class:`.Context` instance.
+        """
+
+        result = self.R.retrieveContext(contexturl)
+
+        self.assertIsInstance(result, Context)
+        self.assertEqual(result.url, contexturl)
+        self.assertGreater(len(result.content), 0)
+        self.assertGreater(len(result.title), 0)
