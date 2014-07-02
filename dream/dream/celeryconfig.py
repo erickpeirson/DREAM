0490ef716473b51d3b09002ee3f46166a03db2d6
Erick <erick.peirson@asu.edu>
2014-07-01 18:11:49 -0700

    bugfixes and configuration

--- dream/dream/celeryconfig.py
+++ dream/dream/celeryconfig.py
@@ -1,6 +1,9 @@
 ## Broker settings.
 BROKER_URL = 'amqp://dream:dreamer@localhost:5672/dreamhost'
 CELERY_RESULT_BACKEND = "amqp"
+
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+CELERY_CHORD_PROPAGATES = True
 #CELERY_TASK_SERIALIZER = "json"
 #CELERY_ACCEPT_CONTENT = ['json', 'msgpack', 'yaml']
 
